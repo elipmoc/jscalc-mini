5db302ec485137b6f92d2754a7cb4bea548ac4a2
elipmoc <relipmoc101@gmail.com>
2019-02-18 02:11:06 +0900

    よりわかりやすく

--- main.js
+++ main.js
@@ -4,30 +4,48 @@ document.getElementById("button").onclick = buttonClick;
 
 //計算ボタンが押された時に呼ばれる関数
 function buttonClick() {
-    const results = calc(document.getElementById("input").value);
-    const tokenResult =
-        document.getElementById("tokenResult");
-    tokenResult.value = "";
-    for (const token of results.tokenList)
-        tokenResult.value += `${token.tokenType} : ${token.value}\r\n`;
-    document.getElementById("calcResult").value = results.calcResult;
+    // document.getElementById("タグid").value でテキストボックスなどに入力された値を取得できる。
+    const result = calc(document.getElementById("input").value);
+    document.getElementById("tokenResult").value = tokenListToString(result.tokenList);
+    document.getElementById("calcResult").value = result.calcResult;
+}
+
+//tokenListを文字列に結合して返す
+function tokenListToString(tokenList) {
+    let str = "";
+    for (const token of tokenList)
+        //JavaScriptのテンプレートリテラルという機能を使用「`」はバッククォートと読む
+        str += `${token.tokenType} : ${token.value}\r\n`;
+    return str;
+
+    /*別解
+
+    たった一行で書けるスマートな方法
+    return tokenList.reduce((acc, token) => acc + `${token.tokenType} : ${token.value}\r\n`, "");
+
+    */
 }
 
 //文字列を解析して計算結果を返す関数
 function calc(inputStr) {
+    //トークンリストの取得
     const tokenList = lexicalAnalysis(inputStr);
     if (tokenList === null)
         return { tokenList: [], calcResult: "字句解析が失敗しました" };
+    //式木の取得
     const exprTree = syntacticAnalysis(tokenList);
     if (exprTree === null)
-        return { tokenList, calcResult: "構文解析が失敗しました" };
-    return { tokenList, calcResult: exprTree.result() };
+        return { tokenList: tokenList, calcResult: "構文解析が失敗しました" };
+    //計算結果を返す
+    return { tokenList: tokenList, calcResult: exprTree.result() };
+    // return { tokenList, calcResult: exprTree.result() };でもOK(キーと変数名が一緒なら省略できる)
 }
 
 
 //字句解析---------------------------------------------------------------------------
 function lexicalAnalysis(inputStr) {
     const tokenList = [];
+    // 文字列の長さが0になるまで切り出し続ける
     while (inputStr.length !== 0) {
         if (addToken(numCut()));
         else if (addToken(opCut()));
@@ -38,19 +56,19 @@ function lexicalAnalysis(inputStr) {
     //トークンの追加
     function addToken(parseResult) {
         if (parseResult !== null) {
-            tokenList.push({
-                value: parseResult.value,
-                tokenType: parseResult.tokenType
-            });
+            tokenList.push(parseResult);
             return true;
         }
         return false;
     }
 
-    //数字の切り出し
+    //数値トークンの切り出し
+    //トークンオブジェクトかnullのどちらかを返す
     function numCut() {
         let i = 0;
         let buf = "";
+        //isNaNで文字が数字かどうか判定できる
+        //数字じゃなくなるまでbufに詰め続ける
         while (isNaN(inputStr[i]) === false) {
             buf += inputStr[i];
             i++;
@@ -60,13 +78,15 @@ function lexicalAnalysis(inputStr) {
                 value: Number(buf),
                 tokenType: "num"
             };
+            //slice(x)で先頭からx個だけ取り除いた文字列を取得できる
             inputStr = inputStr.slice(i);
             return token;
         }
         return null;
     }
 
-    //演算子の切り出し
+    //演算子トークンの切り出し
+    //トークンオブジェクトかnullのどちらかを返す
     function opCut() {
         if (
             ["+", "-", "*", "/"].includes(inputStr[0])
@@ -75,6 +95,7 @@ function lexicalAnalysis(inputStr) {
                 value: inputStr[0],
                 tokenType: "op"
             };
+            //slice(x)で先頭からx個だけ取り除いた文字列を取得できる
             inputStr = inputStr.slice(1);
             return token;
         }
@@ -110,6 +131,7 @@ class OpExpr {
         this.opChar = opChar;
     }
     result() {
+        //JavaScriptの分割代入という機能を使用
         const [left, right] = [this.leftExpr.result(), this.rightExpr.result()];
         switch (this.opChar) {
             case "+":
@@ -130,6 +152,7 @@ function syntacticAnalysis(tokenList) {
     return exprParser();
 
     //<expr>の構文解析
+    //OpExprかnullのどちらかを返す
     function exprParser() {
         let expr = termParser();
         if (expr === null) return null;
@@ -145,6 +168,7 @@ function syntacticAnalysis(tokenList) {
     }
 
     //<term>の構文解析
+    //OpExprかnullのどちらかを返す
     function termParser() {
         let expr = numParser();
         if (expr === null) return null;
@@ -160,6 +184,7 @@ function syntacticAnalysis(tokenList) {
     }
 
     //<num>の構文解析
+    //NumExprかnullのどちらかを返す
     function numParser() {
         const token = tokenList[index];
         if (tokenList.length > index && token.tokenType === "num") {
